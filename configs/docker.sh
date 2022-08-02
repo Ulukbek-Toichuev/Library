8cfd38143df1ed21a55282eef71102a964728be2
Toychuev. Ulukbek <67442103+Ulukbek-Toychuev@users.noreply.github.com>
2022-08-02 12:35:46 +0300

    Add bash script

---
+++ configs/docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Installing docker"
+
+
+sudo apt-get update
+
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+groupadd docker
+$1
+usermod -aG docker $1
+
+sudo reboot
